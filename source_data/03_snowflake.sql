c7e20f74441be4bbae86138af4c8e457393a238b
Michael Tropin <mihtrop2005@yandex.ru>
2026-03-22 17:15:34 +0300

    fixes

--- source_data/03_snowflake.sql
+++ source_data/03_snowflake.sql
@@ -1,130 +1,152 @@
-CREATE SCHEMA snowflake;
+CREATE SCHEMA IF NOT EXISTS snowflake;
 
-CREATE TABLE snowflake.dim_pet_types (
-    pet_type_id SERIAL PRIMARY KEY,
-    name VARCHAR(50)
-);
+CREATE TABLE
+    snowflake.dim_countries (
+        country_id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL UNIQUE
+    );
 
-CREATE TABLE snowflake.dim_pet_breeds (
-    pet_breed_id SERIAL PRIMARY KEY,
-    pet_type_id INTEGER REFERENCES snowflake.dim_pet_types(pet_type_id) ON DELETE SET NULL,
-    name VARCHAR(100)
-);
+CREATE TABLE
+    snowflake.dim_states (
+        state_id SERIAL PRIMARY KEY,
+        country_id INTEGER REFERENCES snowflake.dim_countries (country_id),
+        name VARCHAR(100) NOT NULL,
+        UNIQUE (name, country_id)
+    );
 
-CREATE TABLE snowflake.dim_pets (
-    pet_id SERIAL PRIMARY KEY,
-    name VARCHAR(100),
-    breed_id INTEGER REFERENCES snowflake.dim_pet_breeds(pet_breed_id) ON DELETE SET NULL
-);
+CREATE TABLE
+    snowflake.dim_cities (
+        city_id SERIAL PRIMARY KEY,
+        state_id INTEGER REFERENCES snowflake.dim_states (state_id),
+        name VARCHAR(100) NOT NULL,
+        UNIQUE (name, state_id)
+    );
 
-CREATE TABLE snowflake.dim_countries (
-    country_id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
-);
+CREATE TABLE
+    snowflake.dim_pet_types (
+        pet_type_id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL UNIQUE
+    );
 
-CREATE TABLE snowflake.dim_states (
-    state_id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
-);
+CREATE TABLE
+    snowflake.dim_pet_breeds (
+        pet_breed_id SERIAL PRIMARY KEY,
+        pet_type_id INTEGER REFERENCES snowflake.dim_pet_types (pet_type_id),
+        name VARCHAR(100) NOT NULL,
+        UNIQUE (name, pet_type_id)
+    );
 
-CREATE TABLE snowflake.dim_cities (
-    city_id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
-);
+CREATE TABLE
+    snowflake.dim_pets (
+        pet_id SERIAL PRIMARY KEY,
+        name VARCHAR(100),
+        breed_id INTEGER REFERENCES snowflake.dim_pet_breeds (pet_breed_id)
+    );
 
-CREATE TABLE snowflake.dim_customers (
-    customer_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    email VARCHAR(255),
-    age INTEGER,
-    country_id INTEGER REFERENCES snowflake.dim_countries(country_id) ON DELETE SET NULL,
-    postal_code VARCHAR(20),
-    pet_id INTEGER REFERENCES snowflake.dim_pets(pet_id) ON DELETE SET NULL
-);
+CREATE TABLE
+    snowflake.dim_product_categories (
+        category_id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL UNIQUE
+    );
 
-CREATE TABLE snowflake.dim_sellers (
-    seller_id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100),
-    last_name VARCHAR(100),
-    email VARCHAR(255),
-    country_id INTEGER REFERENCES snowflake.dim_countries(country_id) ON DELETE SET NULL,
-    postal_code VARCHAR(20)
-);
+CREATE TABLE
+    snowflake.dim_pet_categories (
+        pet_category_id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL UNIQUE
+    );
 
-CREATE TABLE snowflake.dim_stores (
-    store_id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    location VARCHAR(255),
-    country_id INTEGER REFERENCES snowflake.dim_countries(country_id) ON DELETE SET NULL,
-    state_id INTEGER REFERENCES snowflake.dim_states(state_id) ON DELETE SET NULL,
-    city_id INTEGER REFERENCES snowflake.dim_cities(city_id) ON DELETE SET NULL,
-    phone VARCHAR(50),
-    email VARCHAR(255)
-);
+CREATE TABLE
+    snowflake.dim_colors (
+        color_id SERIAL PRIMARY KEY,
+        name VARCHAR(50) NOT NULL UNIQUE
+    );
 
-CREATE TABLE snowflake.dim_product_categories (
-    category_id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
-);
+CREATE TABLE
+    snowflake.dim_brands (
+        brand_id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL UNIQUE
+    );
 
-CREATE TABLE snowflake.dim_pet_categories (
-    pet_category_id SERIAL PRIMARY KEY,
-    name VARCHAR(50)
-);
+CREATE TABLE
+    snowflake.dim_materials (
+        material_id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL UNIQUE
+    );
 
-CREATE TABLE snowflake.dim_suppliers (
-    supplier_id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    contact VARCHAR(255),
-    email VARCHAR(255),
-    phone VARCHAR(50),
-    address TEXT,
-    city_id INTEGER REFERENCES snowflake.dim_cities(city_id) ON DELETE SET NULL,
-    country_id INTEGER REFERENCES snowflake.dim_countries(country_id) ON DELETE SET NULL
-);
+CREATE TABLE
+    snowflake.dim_suppliers (
+        supplier_id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL UNIQUE,
+        contact VARCHAR(255),
+        email VARCHAR(255) UNIQUE,
+        phone VARCHAR(50),
+        address TEXT,
+        city_id INTEGER REFERENCES snowflake.dim_cities (city_id)
+    );
 
-CREATE TABLE snowflake.dim_colors (
-    color_id SERIAL PRIMARY KEY,
-    name VARCHAR(50)
-);
+CREATE TABLE
+    snowflake.dim_customers (
+        customer_id SERIAL PRIMARY KEY,
+        first_name VARCHAR(100),
+        last_name VARCHAR(100),
+        email VARCHAR(255) NOT NULL UNIQUE,
+        age INTEGER,
+        city_id INTEGER REFERENCES snowflake.dim_cities (city_id),
+        postal_code VARCHAR(20),
+        pet_id INTEGER REFERENCES snowflake.dim_pets (pet_id)
+    );
 
-CREATE TABLE snowflake.dim_brands (
-    brand_id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
-);
+CREATE TABLE
+    snowflake.dim_sellers (
+        seller_id SERIAL PRIMARY KEY,
+        first_name VARCHAR(100),
+        last_name VARCHAR(100),
+        email VARCHAR(255) NOT NULL UNIQUE,
+        city_id INTEGER REFERENCES snowflake.dim_cities (city_id),
+        postal_code VARCHAR(20)
+    );
 
-CREATE TABLE snowflake.dim_materials (
-    material_id SERIAL PRIMARY KEY,
-    name VARCHAR(100)
-);
+CREATE TABLE
+    snowflake.dim_stores (
+        store_id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        location VARCHAR(255),
+        city_id INTEGER REFERENCES snowflake.dim_cities (city_id),
+        phone VARCHAR(50),
+        email VARCHAR(255) UNIQUE
+    );
 
-CREATE TABLE snowflake.dim_products (
-    product_id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    pet_category_id INTEGER REFERENCES snowflake.dim_pet_categories(pet_category_id) ON DELETE SET NULL,
-    category_id INTEGER REFERENCES snowflake.dim_product_categories(category_id) ON DELETE SET NULL,
-    price DECIMAL(10,2),
-    weight DECIMAL(10,2),
-    color_id INTEGER REFERENCES snowflake.dim_colors(color_id) ON DELETE SET NULL,
-    size VARCHAR(50),
-    brand_id INTEGER REFERENCES snowflake.dim_brands(brand_id) ON DELETE SET NULL,
-    material_id INTEGER REFERENCES snowflake.dim_materials(material_id) ON DELETE SET NULL,
-    description TEXT,
-    rating DECIMAL(3,1),
-    reviews INTEGER,
-    release_date DATE,
-    expiry_date DATE,
-    supplier_id INTEGER REFERENCES snowflake.dim_suppliers(supplier_id) ON DELETE SET NULL
-);
+CREATE TABLE
+    snowflake.dim_products (
+        product_id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        pet_category_id INTEGER REFERENCES snowflake.dim_pet_categories (pet_category_id),
+        category_id INTEGER REFERENCES snowflake.dim_product_categories (category_id),
+        price DECIMAL(10, 2),
+        weight DECIMAL(10, 2),
+        color_id INTEGER REFERENCES snowflake.dim_colors (color_id),
+        size VARCHAR(50),
+        brand_id INTEGER REFERENCES snowflake.dim_brands (brand_id),
+        material_id INTEGER REFERENCES snowflake.dim_materials (material_id),
+        description TEXT,
+        rating DECIMAL(3, 1),
+        reviews INTEGER,
+        release_date DATE,
+        expiry_date DATE,
+        supplier_id INTEGER REFERENCES snowflake.dim_suppliers (supplier_id)
+    );
 
-CREATE TABLE snowflake.fact_sales (
-    sale_id SERIAL PRIMARY KEY,
-    customer_id INTEGER REFERENCES snowflake.dim_customers(customer_id) ON DELETE SET NULL,
-    seller_id INTEGER REFERENCES snowflake.dim_sellers(seller_id) ON DELETE SET NULL,
-    product_id INTEGER REFERENCES snowflake.dim_products(product_id) ON DELETE SET NULL,
-    store_id INTEGER REFERENCES snowflake.dim_stores(store_id) ON DELETE SET NULL,
-    quantity INTEGER,
-    total_price DECIMAL(10,2),
-    date DATE
-);
+CREATE TABLE
+    snowflake.fact_sales (
+        sale_id SERIAL PRIMARY KEY,
+        customer_id INTEGER REFERENCES snowflake.dim_customers (customer_id),
+        seller_id INTEGER REFERENCES snowflake.dim_sellers (seller_id),
+        product_id INTEGER REFERENCES snowflake.dim_products (product_id),
+        store_id INTEGER REFERENCES snowflake.dim_stores (store_id),
+        quantity INTEGER,
+        total_price DECIMAL(10, 2),
+        date DATE,
+        UNIQUE (customer_id, product_id, date)
+    );
+
+\echo 'All created'
--- source_data/04_dml.sql
+++ source_data/04_dml.sql
@@ -1,186 +1,258 @@
-INSERT INTO snowflake.dim_pet_types (name)
-SELECT DISTINCT 
-    customer_pet_type 
-FROM mock_data 
-WHERE customer_pet_type IS NOT NULL;
+INSERT INTO
+    snowflake.dim_pet_types (name)
+SELECT DISTINCT
+    customer_pet_type
+FROM
+    mock_data
+WHERE
+    customer_pet_type IS NOT NULL ON CONFLICT DO NOTHING;
 
-INSERT INTO snowflake.dim_pet_breeds (pet_type_id, name)
+INSERT INTO
+    snowflake.dim_pet_breeds (pet_type_id, name)
 SELECT DISTINCT
     pt.pet_type_id,
-    m.customer_pet_breed 
-FROM mock_data m
-LEFT JOIN snowflake.dim_pet_types pt ON pt.name = m.customer_pet_type 
-WHERE m.customer_pet_breed IS NOT NULL;
+    m.customer_pet_breed
+FROM
+    mock_data m
+    JOIN snowflake.dim_pet_types pt ON pt.name = m.customer_pet_type ON CONFLICT DO NOTHING;
 
-INSERT INTO snowflake.dim_pets (pet_id, breed_id, name)
+INSERT INTO
+    snowflake.dim_pets (pet_id, breed_id, name)
 SELECT DISTINCT
     m.sale_customer_id + FLOOR((m.id - 1) / 1000) * 1000,
     pb.pet_breed_id,
-    m.customer_pet_breed 
-FROM mock_data m
-LEFT JOIN snowflake.dim_pet_types pt ON pt.name = m.customer_pet_type 
-LEFT JOIN snowflake.dim_pet_breeds pb ON pb.name = m.customer_pet_breed  AND pb.pet_type_id = pt.pet_type_id
-WHERE m.customer_pet_name IS NOT NULL AND m.sale_customer_id IS NOT NULL;
+    m.customer_pet_name
+FROM
+    mock_data m
+    JOIN snowflake.dim_pet_types pt ON pt.name = m.customer_pet_type
+    JOIN snowflake.dim_pet_breeds pb ON pb.name = m.customer_pet_breed
+    AND pb.pet_type_id = pt.pet_type_id ON CONFLICT DO NOTHING;
 
-INSERT INTO snowflake.dim_countries (name)
+INSERT INTO
+    snowflake.dim_countries (name)
 SELECT DISTINCT
-    name 
-FROM (
-    (SELECT DISTINCT customer_country AS name FROM mock_data WHERE customer_country IS NOT NULL)
-    UNION
-    (SELECT DISTINCT seller_country AS name FROM mock_data WHERE seller_country IS NOT NULL)
-    UNION
-    (SELECT DISTINCT store_country AS name FROM mock_data WHERE store_country IS NOT NULL)
-    UNION
-    (SELECT DISTINCT supplier_country AS name FROM mock_data WHERE supplier_country IS NOT NULL)
-) as tbl;
+    val
+FROM
+    (
+        SELECT
+            customer_country AS val
+        FROM
+            mock_data
+        UNION
+        SELECT
+            store_country
+        FROM
+            mock_data
+        UNION
+        SELECT
+            supplier_country
+        FROM
+            mock_data
+    ) t
+WHERE
+    val IS NOT NULL ON CONFLICT DO NOTHING;
 
-
-INSERT INTO snowflake.dim_states (name)
+INSERT INTO
+    snowflake.dim_states (name, country_id)
 SELECT DISTINCT
-    store_state 
-FROM mock_data
-WHERE store_state IS NOT NULL;
+    m.store_state,
+    c.country_id
+FROM
+    mock_data m
+    JOIN snowflake.dim_countries c ON c.name = m.store_country
+WHERE
+    m.store_state IS NOT NULL
+UNION
+SELECT
+    'N/A',
+    country_id
+FROM
+    snowflake.dim_countries ON CONFLICT DO NOTHING;
 
-INSERT INTO snowflake.dim_cities (name)
+INSERT INTO
+    snowflake.dim_cities (name, state_id)
 SELECT DISTINCT
-    name 
-FROM (
-    (SELECT DISTINCT store_city AS name FROM mock_data WHERE store_city IS NOT NULL)
-    UNION
-    (SELECT DISTINCT supplier_city AS name FROM mock_data WHERE supplier_city IS NOT NULL)
-) AS tbl;
+    m.store_city,
+    s.state_id
+FROM
+    mock_data m
+    JOIN snowflake.dim_countries c ON c.name = m.store_country
+    JOIN snowflake.dim_states s ON s.name = m.store_state
+    AND s.country_id = c.country_id ON CONFLICT DO NOTHING;
 
-INSERT INTO snowflake.dim_customers (customer_id, first_name, last_name, email, age, country_id, postal_code, pet_id)
+INSERT INTO
+    snowflake.dim_cities (name, state_id)
+SELECT DISTINCT
+    m.supplier_city,
+    s.state_id
+FROM
+    mock_data m
+    JOIN snowflake.dim_countries c ON c.name = m.supplier_country
+    JOIN snowflake.dim_states s ON s.name = 'N/A'
+    AND s.country_id = c.country_id ON CONFLICT DO NOTHING;
+
+INSERT INTO
+    snowflake.dim_cities (name, state_id)
+SELECT
+    'Unknown City',
+    state_id
+FROM
+    snowflake.dim_states ON CONFLICT DO NOTHING;
+
+INSERT INTO
+    snowflake.dim_suppliers (name, contact, email, phone, address, city_id)
+SELECT DISTINCT
+    m.supplier_name,
+    m.supplier_contact,
+    m.supplier_email,
+    m.supplier_phone,
+    m.supplier_address,
+    ci.city_id
+FROM
+    mock_data m
+    JOIN snowflake.dim_countries co ON co.name = m.supplier_country
+    JOIN snowflake.dim_states st ON st.country_id = co.country_id
+    JOIN snowflake.dim_cities ci ON ci.name = m.supplier_city
+    AND ci.state_id = st.state_id ON CONFLICT DO NOTHING;
+
+INSERT INTO
+    snowflake.dim_customers (
+        customer_id,
+        first_name,
+        last_name,
+        email,
+        age,
+        city_id,
+        postal_code,
+        pet_id
+    )
 SELECT DISTINCT
     m.sale_customer_id + FLOOR((m.id - 1) / 1000) * 1000,
     m.customer_first_name,
     m.customer_last_name,
     m.customer_email,
     m.customer_age,
-    cntr.country_id,
+    ci.city_id,
     m.customer_postal_code,
     m.sale_customer_id + FLOOR((m.id - 1) / 1000) * 1000
 FROM
     mock_data m
-LEFT JOIN snowflake.dim_countries cntr ON cntr.name = m.customer_country
-WHERE m.sale_customer_id IS NOT NULL;
+    JOIN snowflake.dim_countries co ON co.name = m.customer_country
+    JOIN snowflake.dim_states st ON st.name = 'N/A'
+    AND st.country_id = co.country_id
+    JOIN snowflake.dim_cities ci ON ci.name = 'Unknown City'
+    AND ci.state_id = st.state_id
+WHERE
+    m.sale_customer_id IS NOT NULL ON CONFLICT DO NOTHING;
 
-INSERT INTO snowflake.dim_sellers (seller_id, first_name, last_name, email, country_id, postal_code)
-SELECT 
-    DISTINCT m.sale_seller_id + FLOOR((m.id - 1) / 1000) * 1000,
+INSERT INTO
+    snowflake.dim_sellers (
+        seller_id,
+        first_name,
+        last_name,
+        email,
+        city_id,
+        postal_code
+    )
+SELECT DISTINCT
+    m.sale_seller_id + FLOOR((m.id - 1) / 1000) * 1000,
     m.seller_first_name,
     m.seller_last_name,
     m.seller_email,
-    cntr.country_id,
+    (
+        SELECT
+            city_id
+        FROM
+            snowflake.dim_cities
+        WHERE
+            name = 'Unknown City'
+        LIMIT
+            1
+    ),
     m.seller_postal_code
 FROM
     mock_data m
-LEFT JOIN snowflake.dim_countries cntr ON cntr.name = m.seller_country
-WHERE m.sale_seller_id IS NOT NULL;
+WHERE
+    m.sale_seller_id IS NOT NULL ON CONFLICT DO NOTHING;
 
-INSERT INTO snowflake.dim_stores (name, location, country_id, state_id, city_id, phone, email)
-SELECT 
-    DISTINCT m.store_name,
-    m.store_location,
-    cntr.country_id,
-    st.state_id,
-    c.city_id,
-    m.store_phone,
-    m.store_email
-FROM 
-    mock_data AS m
-LEFT JOIN snowflake.dim_countries cntr ON cntr.name = m.store_country
-LEFT JOIN snowflake.dim_states st ON st.name = m.store_state
-LEFT JOIN snowflake.dim_cities c ON c.name = m.store_city
-WHERE m.store_name IS NOT NULL;
-
-INSERT INTO snowflake.dim_product_categories (name)
-SELECT product_category FROM mock_data WHERE product_category IS NOT NULL;
-
-INSERT INTO snowflake.dim_pet_categories (name)
-SELECT m.pet_category 
-FROM mock_data AS m
-WHERE m.pet_category IS NOT NULL;
-
-INSERT INTO snowflake.dim_suppliers (name, contact, email, phone, address, city_id, country_id)
-SELECT 
-    DISTINCT
-    m.supplier_name,
-    m.supplier_contact,
-    m.supplier_email,
-    m.supplier_phone,
-    m.supplier_address,
-    ct.city_id,
-    cntr.country_id
-FROM mock_data AS m
-LEFT JOIN snowflake.dim_cities ct ON ct.name = m.supplier_city
-LEFT JOIN snowflake.dim_countries cntr ON cntr.name = m.supplier_country
-WHERE m.supplier_name IS NOT NULL;
-
-INSERT INTO snowflake.dim_colors (name)
-SELECT DISTINCT m.product_color
-FROM mock_data AS m WHERE m.product_color IS NOT NULL;
-
-INSERT INTO snowflake.dim_brands (name)
-SELECT DISTINCT m.product_brand
-FROM mock_data AS m WHERE m.product_brand IS NOT NULL;
-
-INSERT INTO snowflake.dim_materials (name)
-SELECT DISTINCT m.product_material
-FROM mock_data AS m WHERE m.product_material IS NOT NULL;
-
-WITH prepared_data AS (
-    SELECT 
-        m.sale_product_id + FLOOR((m.id - 1) / 1000) * 1000 AS product_id,
-        m.product_name,
-        m.product_price,
-        m.product_weight,
-        m.product_size,
-        m.product_description,
-        m.product_rating,
-        m.product_reviews,
-        m.product_release_date,
-        m.product_expiry_date,
-        m.supplier_email,
-        m.pet_category,
-        m.product_category,
-        m.product_color,
-        m.product_material,
-        b.brand_id
-    FROM mock_data m
-    LEFT JOIN snowflake.dim_brands b ON b.name = m.product_brand
-    WHERE m.sale_product_id IS NOT NULL
-)
-INSERT INTO snowflake.dim_products (
-    product_id, name, pet_category_id, category_id, price, weight, 
-    color_id, size, brand_id, material_id, description, 
-    rating, reviews, release_date, expiry_date, supplier_id
-)
+INSERT INTO
+    snowflake.dim_products (
+        product_id,
+        name,
+        price,
+        weight,
+        color_id,
+        size,
+        brand_id,
+        material_id,
+        release_date,
+        supplier_id
+    )
 SELECT DISTINCT
-    pd.product_id,
-    pd.product_name,
-    (SELECT pc.pet_category_id FROM snowflake.dim_pet_categories pc WHERE pc.name = pd.pet_category LIMIT 1),
-    (SELECT cat.category_id FROM snowflake.dim_product_categories cat WHERE cat.name = pd.product_category LIMIT 1),
-    pd.product_price,
-    pd.product_weight,
-    (SELECT col.color_id FROM snowflake.dim_colors col WHERE col.name = pd.product_color LIMIT 1),
-    pd.product_size,
-    pd.brand_id,
-    (SELECT mat.material_id FROM snowflake.dim_materials mat WHERE mat.name = pd.product_material LIMIT 1),
-    pd.product_description,
-    pd.product_rating,
-    pd.product_reviews,
-    CASE WHEN pd.product_release_date ~ '^\d{1,2}/\d{1,2}/\d{4}$' 
-         THEN to_date(pd.product_release_date, 'FMMM/FMDD/YYYY') 
-         ELSE NULL END,
-    CASE WHEN pd.product_expiry_date ~ '^\d{1,2}/\d{1,2}/\d{4}$' 
-         THEN to_date(pd.product_expiry_date, 'FMMM/FMDD/YYYY') 
-         ELSE NULL END,
-    (SELECT sup.supplier_id FROM snowflake.dim_suppliers sup WHERE sup.email = pd.supplier_email LIMIT 1)
-FROM prepared_data pd;
+    m.sale_product_id + FLOOR((m.id - 1) / 1000) * 1000,
+    m.product_name,
+    m.product_price,
+    m.product_weight,
+    (
+        SELECT
+            color_id
+        FROM
+            snowflake.dim_colors
+        WHERE
+            name = m.product_color
+        LIMIT
+            1
+    ),
+    m.product_size,
+    (
+        SELECT
+            brand_id
+        FROM
+            snowflake.dim_brands
+        WHERE
+            name = m.product_brand
+        LIMIT
+            1
+    ),
+    (
+        SELECT
+            material_id
+        FROM
+            snowflake.dim_materials
+        WHERE
+            name = m.product_material
+        LIMIT
+            1
+    ),
+    CASE
+        WHEN m.product_release_date ~ '^\d{1,2}/\d{1,2}/\d{4}$' THEN to_date (m.product_release_date, 'FMMM/FMDD/YYYY')
+        ELSE NULL
+    END,
+    (
+        SELECT
+            supplier_id
+        FROM
+            snowflake.dim_suppliers
+        WHERE
+            email = m.supplier_email
+        LIMIT
+            1
+    )
+FROM
+    mock_data m
+WHERE
+    m.sale_product_id IS NOT NULL ON CONFLICT DO NOTHING;
 
-INSERT INTO snowflake.fact_sales (customer_id, seller_id, product_id, store_id, quantity, total_price, date)
+INSERT INTO
+    snowflake.fact_sales (
+        customer_id,
+        seller_id,
+        product_id,
+        store_id,
+        quantity,
+        total_price,
+        date
+    )
 SELECT
     m.sale_customer_id + FLOOR((m.id - 1) / 1000) * 1000,
     m.sale_seller_id + FLOOR((m.id - 1) / 1000) * 1000,
@@ -188,11 +260,12 @@ SELECT
     st.store_id,
     m.sale_quantity,
     m.sale_total_price,
-    CASE WHEN m.sale_date ~ '^\d{1,2}/\d{1,2}/\d{4}$' 
-         THEN to_date(m.sale_date, 'FMMM/FMDD/YYYY') 
-         ELSE NULL END
+    CASE
+        WHEN m.sale_date ~ '^\d{1,2}/\d{1,2}/\d{4}$' THEN to_date (m.sale_date, 'FMMM/FMDD/YYYY')
+        ELSE NULL
+    END
 FROM
     mock_data m
-LEFT JOIN snowflake.dim_stores st ON st.email = m.store_email;
+    JOIN snowflake.dim_stores st ON st.email = m.store_email ON CONFLICT DO NOTHING;
 
 \echo 'All saved'
